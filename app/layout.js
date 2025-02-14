dd4c653ecc66c3f8e2f8d9a165930f32a0b3cfb0
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2025-02-14 12:10:17 +0530

    integrated clerk signin and signup pages

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Outfit, Outfit } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 
 const outfit = Outfit({
@@ -13,10 +14,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={outfit.className}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+          <body className={outfit.className}>
+            {children}
+          </body>
+      </html>
+    </ClerkProvider>
   );
 }
