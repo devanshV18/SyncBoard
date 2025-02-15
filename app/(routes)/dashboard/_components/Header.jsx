7d4949a6cfefd77ea7ae496617c529f0011881e8
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2025-02-15 13:04:23 +0530

    organisation feature added

--- app/(routes)/dashboard/_components/Header.jsx
+++ app/(routes)/dashboard/_components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Logo from '../../../_components/Logo'; // Move up two levels to reach `app/_components/Logo`
-import { UserButton } from '@clerk/nextjs';
+import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
 
 function Header() {
   return (
     <div className='flex justify-between items-center p-6 shadow-md'> 
       <Logo />
+      <OrganizationSwitcher/>
       <UserButton/>
     </div>
   );
