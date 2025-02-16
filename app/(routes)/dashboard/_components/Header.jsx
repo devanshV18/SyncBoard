24cf46c9e63d9c74f89cfa78e3684a3d54a3c675
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2025-02-16 07:11:46 +0530

    fixed routing in org feature

--- app/(routes)/dashboard/_components/Header.jsx
+++ app/(routes)/dashboard/_components/Header.jsx
@@ -1,12 +1,19 @@
+"use client"
 import React from 'react';
 import Logo from '../../../_components/Logo'; // Move up two levels to reach `app/_components/Logo`
-import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
+import { OrganizationSwitcher, useAuth, UserButton } from '@clerk/nextjs';
 
 function Header() {
+
+  const {orgId} = useAuth()
+  console.log(orgId)
   return (
     <div className='flex justify-between items-center p-6 shadow-md'> 
       <Logo />
-      <OrganizationSwitcher/>
+      <OrganizationSwitcher 
+        afterLeaveOrganizationUrl={'/dashboard'} 
+        afterCreateOrganizationUrl={'/dashboard'}
+      />
       <UserButton/>
     </div>
   );
